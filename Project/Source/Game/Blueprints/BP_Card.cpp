7543fa58b60a37f10f8560238ae1af827e33ebcb
tonarinoanoko <tonarinn00@gmail.com>
2026-05-13 22:20:43 +0900

    プロジェクトを設定。

--- Project/Source/Game/Blueprints/BP_Card.cpp
+++ Project/Source/Game/Blueprints/BP_Card.cpp
@@ -1,13 +1,14 @@
 ﻿#include "BP_Card.h"
 #include "Game/Global/ServiceLocator.h"
 #include "Game/MasterData/MasterDataManager.h"
-#include "Game/MasterData/Datas/MD_Card.h"
+//#include "Game/MasterData/Datas/MD_Card.h"
 //#include "Game/Components/Common/Co_Card.h"
 //#include "Game/Systems/Sys_ThunderStorm.h"
 
 namespace MyGame {
 MyECS::Entity BP_Card::Create(MyECS::World& world, MyECS::SystemManager& systems, const std::string& cardId)
 {
+    /*
     // 1. マスタ取得
     auto const masterMgr = ServiceLocator::get<MasterDataManager>();
     auto const cardMaster = masterMgr->get<MD_Card>();
@@ -34,6 +35,7 @@ MyECS::Entity BP_Card::Create(MyECS::World& world, MyECS::SystemManager& systems
     }
     */
 
+    auto entity = world.createEntity();
     return entity;
 }
 }
